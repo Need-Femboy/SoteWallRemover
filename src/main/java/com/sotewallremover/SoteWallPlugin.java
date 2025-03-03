4c434dd6c389d3adbaebdc49cbc67f1d9fee83d7
Need Femboy <no@mail.com>
2025-03-03 19:56:31 +0000

    Nex Support

--- src/main/java/com/sotewallremover/SoteWallPlugin.java
+++ src/main/java/com/sotewallremover/SoteWallPlugin.java
@@ -2,38 +2,36 @@ package com.sotewallremover;
 
 import javax.inject.Inject;
 
-import com.google.common.collect.ImmutableSet;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
-import java.util.Set;
-
 @Slf4j
 @PluginDescriptor(
-		name = "Sote Wall Remover",
-		description = "Removes the annoying wall behind Sotetseg in ToB",
-		tags = {"tob", "theatre of blood"}
+		name = "Boss Wall Remover",
+		description = "Removes annoying walls at Sotetseg and Nex",
+		tags = {"tob", "theatre of blood", "nex", "bossing"}
 )
 public class SoteWallPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 	
-	
-	Set<Integer> WALL_IDS = ImmutableSet.of(33044, 33046, 33047, 33048, 33049, 33050, 33051, 33052, 33059, 33058, 33057, 33056, 33055, 33054, 33053);
+	@Inject
+	private ClientThread clientThread;
 	
 	@Override
 	protected void startUp() throws Exception
 	{
 		//In-case someone starts the plugin whilst already in the room I guess?
-		if (correctRegion())
+		if (client.getGameState() == GameState.LOGGED_IN)
 		{
-			removeWall();
+			clientThread.invoke(this::regionAndWallCheck);
 		}
 	}
 	
@@ -51,49 +49,64 @@ public class SoteWallPlugin extends Plugin
 			return;
 		}
 		
-		if (!correctRegion())
-		{
-			return;
-		}
-		
-		removeWall();
+		regionAndWallCheck();
 	}
 	
-	private void removeWall()
+	private void regionAndWallCheck()
 	{
-		Scene scene = client.getScene();
-		Tile[][][] tiles = scene.getTiles();
+		int regionId = WorldPoint.fromLocalInstance(client, client.getLocalPlayer().getLocalLocation()).getRegionID();
+		Bosses boss = Bosses.inRegion(regionId);
 		
-		for (int x = 0; x < Constants.SCENE_SIZE; ++x)
-			for (int y = 0; y < Constants.SCENE_SIZE; ++y) {
-				Tile tile = tiles[0][x][y];
-				
-				if (tile == null) {
-					continue;
-				}
-				
-				for (GameObject gameObject: tile.getGameObjects()) {
-					if (gameObject == null) {
+		if (boss != null)
+		{
+			removeWall(boss);
+		}
+	}
+	
+	private void removeWall(Bosses boss)
+	{
+		Scene scene = client.getTopLevelWorldView().getScene();
+		Tile[][][] tiles = scene.getTiles();
+		for (int z = 0; z < boss.maxZ; ++z)
+		{
+			for (int x = 0; x < Constants.SCENE_SIZE; ++x)
+			{
+				for (int y = 0; y < Constants.SCENE_SIZE; ++y)
+				{
+					Tile tile = tiles[z][x][y];
+					
+					if (tile == null)
+					{
 						continue;
 					}
 					
-					if (WALL_IDS.contains(gameObject.getId())) {
-						scene.removeGameObject(gameObject);
-						break;
+					if (boss.gameObj != null)
+					{
+						GameObject[] gameObjects = tile.getGameObjects();
+						if (gameObjects != null)
+						{
+							for (GameObject gameObject : gameObjects)
+							{
+								if (gameObject != null && boss.gameObj.contains(gameObject.getId()))
+								{
+									scene.removeGameObject(gameObject);
+									break;
+								}
+							}
+						}
+					}
+					
+					
+					if (boss.wallObj != null)
+					{
+						WallObject wo = tile.getWallObject();
+						if (wo != null && boss.wallObj.contains(wo.getId()))
+						{
+							scene.removeTile(tile);
+						}
 					}
 				}
 			}
-	}
-	
-	private boolean correctRegion()
-	{
-		if (!client.isInInstancedRegion())
-		{
-			return false;
 		}
-		
-		int regionId = WorldPoint.fromLocalInstance(client, client.getLocalPlayer().getLocalLocation()).getRegionID();
-		
-		return regionId == 13123;
 	}
 }
