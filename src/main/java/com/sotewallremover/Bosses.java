174872173c45fa4cedb039af9e8924297d067c47
Need Femboy <no@email.com>
2026-02-07 16:18:40 +0000

    Adds Calvar'ion region to the vetion enum

--- src/main/java/com/sotewallremover/Bosses.java
+++ src/main/java/com/sotewallremover/Bosses.java
@@ -11,7 +11,7 @@ public enum Bosses {
 	SOTE(ImmutableSet.of(13123), 0, 1, true, ImmutableSet.of(33044, 33046, 33047, 33048, 33049, 33050, 33051, 33052, 33059, 33058, 33057, 33056, 33055, 33054, 33053), null, SoteWallConfig::HideSote),
 	GAUNTLET(ImmutableSet.of(7512), 1, 2, true, ImmutableSet.of(36095, 36097, 36098, 36099, 36100, 36103, 36104, 36105), null, SoteWallConfig::HideGauntlet),
 	CORRUPTED_GAUNTLET(ImmutableSet.of(7768), 1, 2, true, ImmutableSet.of(35992, 35994, 35995, 35996, 35997, 36002), null, SoteWallConfig::HideGauntlet),
-	VETION(ImmutableSet.of(13215), 1, 2, false, ImmutableSet.of(46921, 46928, 47176), ImmutableSet.of(20980, 46914, 46915, 46916), SoteWallConfig::HideVetion);
+	VETION(ImmutableSet.of(13215, 7604), 1, 2, false, ImmutableSet.of(46921, 46928, 47176, 46927), ImmutableSet.of(20980, 46914, 46915, 46916), SoteWallConfig::HideVetion);
 
 	private final Set<Integer> regions;
 	public final int minZ;
